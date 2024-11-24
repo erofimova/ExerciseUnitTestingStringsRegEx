c5a2c2c0cfc548a661a94576a8b79bc36fb9820f
erofimova <e.erofimova@gmail.com>
2024-11-24 23:32:20 +0200

    Unit Test String Method: Text Filter

--- TestApp.UnitTests/TextFilterTests.cs
+++ TestApp.UnitTests/TextFilterTests.cs
@@ -6,33 +6,60 @@ namespace TestApp.UnitTests;
 
 public class TextFilterTests
 {
-    // TODO: finish the test
+   
     [Test]
     public void Test_Filter_WhenNoBannedWords_ShouldReturnOriginalText()
     {
         // Arrange
-
+        string[] words = {"blue", "green", "red"};
+        string text = "Colors: yellow, white, black";
+        string expected = "Colors: yellow, white, black";
         // Act
+        string result = TextFilter.Filter(words, text);
 
         // Assert
-        //Assert.That(result, Is.EqualTo(text));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Filter_WhenBannedWordExists_ShouldReplaceBannedWordWithAsterisks()
     {
-        // TODO: finish the test
+        // Arrange
+        string[] words = {"red","blue"};
+        string text = "Colors: yellow, red, white, black";
+        string expected = "Colors: yellow, ***, white, black";
+        // Act   
+        string result = TextFilter.Filter(words, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Filter_WhenBannedWordsAreEmpty_ShouldReturnOriginalText()
     {
-        // TODO: finish the test
+        // Arrange
+        string [] words = Array.Empty<string>();
+        string text = "Colors: yellow, red, white, black";
+        string expected = "Colors: yellow, red, white, black";
+        // Act
+        string result = TextFilter.Filter(words, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Filter_WhenBannedWordsContainWhitespace_ShouldReplaceBannedWord()
     {
-        // TODO: finish the test
+        // Arrange
+        string[] words = {" "};
+        string text = "Colors: yellow, red, white, black";
+        string expected = "Colors:*yellow,*red,*white,*black";
+        // Act
+        string result = TextFilter.Filter(words, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
