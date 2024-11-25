f03705531e6efa5158c8deec87da817e4a525384
erofimova <e.erofimova@gmail.com>
2024-11-26 00:25:15 +0200

    Unit Test RegEx: Match Dates

--- TestApp.UnitTests/MatchDatesTests.cs
+++ TestApp.UnitTests/MatchDatesTests.cs
@@ -1,23 +1,26 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace TestApp.UnitTests;
 
 public class MatchDatesTests
 {
-    // TODO: finish the test
+    
     [Test]
     public void Test_Match_ValidDate_ReturnsExpectedResult()
     {
         // Arrange
+        string input = "31-Dec-2022";
         string expected = "Day: 31, Month: Dec, Year: 2022";
 
         // Act
+        string result = MatchDates.Match(input);
 
         // Assert
-        //Assert.That(result, Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
-    // TODO: finish the test
+
     [Test]
     public void Test_Match_NoMatch_ReturnsEmptyString()
     {
@@ -26,25 +29,47 @@ public class MatchDatesTests
         string expected = string.Empty;
 
         // Act
+        string result = MatchDates.Match(input);
 
         // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Match_MultipleMatches_ReturnsFirstMatch()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "31-December-2022, 12-May-2024, 23 July 2025";
+        string expected = "Day: 31, Month: December, Year: 2022";
+
+        // Act
+        string result = MatchDates.Match(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Match_EmptyString_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = string.Empty;
+        string expected = string.Empty;
+
+        // Act
+        string result = MatchDates.Match(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Match_NullInput_ThrowsArgumentException()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = null;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => MatchDates.Match(input));
     }
 }
